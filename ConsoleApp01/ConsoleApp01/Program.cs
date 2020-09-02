3bf9c404cabe2821dc863abbf23ad76726c89d8c
truong quoc trong <truongquoctrong231194@gmail.com>
2020-09-02 09:23:06 +0700

    update merge 02

--- ConsoleApp01/ConsoleApp01/Program.cs
+++ ConsoleApp01/ConsoleApp01/Program.cs
@@ -16,12 +16,14 @@ namespace ConsoleApp01
             Console.WriteLine("this is v1");
             Console.WriteLine("Ver 03");
             Console.WriteLine("Ver 05");
+            Console.WriteLine("this is Merge 02");
             Console.WriteLine("Ver 06");
             Console.WriteLine("Ver 07");
             Console.WriteLine("Ver 08");
             Console.WriteLine("Ver 09");
             Console.WriteLine("Ver 10");
             Console.WriteLine("Have been Add Development.cs");
+            Console.WriteLine("Have been Add Merge 02.cs");
         }
     }
 }
